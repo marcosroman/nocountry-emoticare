15cd1c6d7a1d58b4783ee1da2990a2ff9e968a67
Angel Añez <angdavid2910@gmail.com>
2024-08-29 20:06:58 -0400

    style: :lipstick: modify height at feature card

--- Frontend/src/components/Card/FeatureCard.tsx
+++ Frontend/src/components/Card/FeatureCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function FeatureCard({ title, desc, children }: Props) {
   return (
-    <article className="border rounded-lg p-4 bg-white">
+    <article className="border rounded-lg p-4 bg-white min-h-44">
       {children}
       <h3 className="font-semibold text-lg mt-3">{title}</h3>
       <p className="mt-4 text-[#6C6E71] text-sm">{desc}</p>

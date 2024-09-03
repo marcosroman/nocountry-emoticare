54b922548c97702c3d48a9688928dd86a4ef3191
Rafael Garcia <rafael000182@gmail.com>
2024-09-02 19:06:44 -0500

    feat: :closed_lock_with_key: missing file from last commit: homepage.tsx

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import FeatureSection from "../components/Section/FeatureSection";
+import HeroSection from "../components/Section/HeroSection";
 
 function HomePage() {
   return (
     <>
+    <HeroSection />
       <FeatureSection />
     </>
   );

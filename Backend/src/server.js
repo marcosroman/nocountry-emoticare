86da7f95b1e39ee3d1d6144545d79a344aea8ae0
Erich Armijo <eothandlock@gmail.com>
2024-09-04 04:20:22 -0400

    feat: :sparkles: enable CORS in Express configuration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 import 'dotenv/config';
 import configureRoutes from './routes/index.js'
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 configureRoutes(app)
 app.listen(process.env.PORT,console.log(`Corriendo en  http://localhost:${process.env.PORT}`));

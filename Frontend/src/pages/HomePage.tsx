8ad8ed1bf42e8b36327c07bae8a43ca4870543a1
Angel Añez <angdavid2910@gmail.com>
2024-09-05 17:19:51 -0400

    feat: :sparkles: add Why Us Section in Home Page

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import FeatureSection from "../components/Section/FeatureSection";
 import HeroSection from "../components/Section/HeroSection";
 import TestimonialsSection from "../components/Section/TestimonialsSection.js";
-
-
+import WhyUsSection from "../components/Section/WhyUsSection.js";
 
 function HomePage() {
   return (
     <>
       <HeroSection />
       <FeatureSection />
+      <WhyUsSection />
       <TestimonialsSection />
     </>
   );

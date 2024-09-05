b0771d6ed73d04c7fe5270aef50b254c827e0040
@RupertoTacuche <murcio2490@gmail.com>
2024-09-04 20:48:05 -0600

    feat: :sparkles: add Testimonials section in Home Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 
+
 function App() {
   return (
     <BrowserRouter>

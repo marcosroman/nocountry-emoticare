687ae81eed656136a69f3fceb4a6646b9805018e
Erich Armijo <eothandlock@gmail.com>
2024-09-06 16:36:37 -0400

    feat: :sparkles: add routes for admin management, doctors, specialties, and profile

---
+++ Backend/src/routes/admin.routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { isAuthenticated } from '../middlewares/authMiddleware.js'
+import { isAdmin } from '../middlewares/adminMiddleware.js'
+import { getAdminByNroDocumentoController, getAllDoctorController } from '../controllers/adminController.js'
+import validateRegister from '../middlewares/userMiddleware.js'
+import createUserController from '../controllers/userController.js'
+import { getAllSpecialtyController } from '../controllers/specialtyController.js'
+
+const router = Router()
+
+router.get('/admin', isAuthenticated, isAdmin,(req,res)=>{
+  res.send('Bienvenido '+ req.user.nombre)
+})
+
+router.get('/admin/doctors', isAuthenticated, isAdmin, getAllDoctorController)
+router.post('/admin/newdoctor', isAuthenticated, isAdmin, validateRegister, createUserController)
+router.get('/admin/doctor/specialty', isAuthenticated, isAdmin, getAllSpecialtyController )
+router.get('/admin/profile', isAuthenticated, isAdmin, getAdminByNroDocumentoController)
+export default router

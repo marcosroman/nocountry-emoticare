19c2dfe9c3cb7325c601383d302967ad3f34fdab
Dan <danderbasp@gmail.com>
2024-09-08 16:45:24 -0400

    feat: added horarios and agendamientos routes

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,10 +1,15 @@
 import registerRoutes from './register.routes.js'
 import loginRoutes from './login.routes.js'
+import horariosRoutes from './horarios.routes.js'
+import agendamientosRoutes from './agendamientos.routes.js'
 import swaggerUI from 'swagger-ui-express'
 import specs from '../swagger/swagger.js'
 console.log(specs)
+
 export default (app) => {
   app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
   app.use(registerRoutes)
   app.use(loginRoutes)
+  app.use(horariosRoutes)
+  app.use(agendamientosRoutes)
 }

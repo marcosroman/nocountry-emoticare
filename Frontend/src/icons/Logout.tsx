6179fac105a650934708f7dd24deb6c6d3d8372c
Angel Añez <angdavid2910@gmail.com>
2024-09-09 17:32:20 -0400

    chore: :bento: add icons for users sidebar

---
+++ Frontend/src/icons/Logout.tsx
@@ -0,0 +1,25 @@
+import { SVGProps } from "react";
+
+function LogoutIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+      <path d="M15 12h-12l3 -3" />
+      <path d="M6 15l-3 -3" />
+    </svg>
+  );
+}
+
+export default LogoutIcon;

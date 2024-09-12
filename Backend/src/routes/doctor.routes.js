3763dc85f790d0f08e6238307991667aa9f82c45
Angel Añez <angdavid2910@gmail.com>
2024-09-11 23:16:41 -0400

    chore: :passport_control: change response messages in admin and doctor routes

--- Backend/src/routes/doctor.routes.js
+++ Backend/src/routes/doctor.routes.js
@@ -4,9 +4,9 @@ const router = Router()
 
 router.get('/doctor', (req, res) => {
   if (req.isAuthenticated() && req.user.rol === 'medico') {
-    res.send('hola ' + req.user.nombre)
+    res.status(200).send({message: "Autorizado"})
   } else {
-    res.status(401).send({ msg: "no autorizado" })
+    res.status(401).send({ error: "No autorizado" })
   }
   console.log(req.session)
   console.log(req.user)

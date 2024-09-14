43a82db43cc43406fbf9e12ad2c9a71e497ae52e
Dan <danderbasp@gmail.com>
2024-09-14 16:00:17 -0400

    chore(agendamientos): removed extra state INICIADO

--- Backend/src/config/dbStructure.sql
+++ Backend/src/config/dbStructure.sql
@@ -139,7 +139,7 @@ CREATE TABLE horarios (
 );
 
 -- agendamientos: id, id_medico, id_paciente, fechahora_inicio, fechahora_fin, estado, fechahora_creado, fechahora_actualizado
-CREATE TYPE estado_agendamiento AS ENUM ('RESERVADO', 'INICIADO', 'FINALIZADO', 'CANCELADO');
+CREATE TYPE estado_agendamiento AS ENUM ('RESERVADO', 'COMPLETADO', 'CANCELADO');
 CREATE TABLE agendamientos (
 	id SERIAL PRIMARY KEY,
 	id_medico INTEGER REFERENCES medicos(id),
